316a28e992560219df56166533bf640c596ba109
vrkajave1994 <99703905+vrushalikajave@users.noreply.github.com>
2022-05-25 16:11:29 +0530

    commit
    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import React from 'react';
 
 
 //   let A=()=> <div> Component : A</div>
@@ -21,30 +22,53 @@ import { useEffect, useState } from 'react';
 // export default App;
 
 
+//************************************************** */
+
+
+
+// function App() {
+// const[count1, setcount1]= useState(0);
+// const[count2, setcount2]= useState(0);
+
+// //useEffect will run always when any componnet mounted
+// // useEffect(()=>{
+// //   console.log("inside useEffect", count1, count2);
+// // })
+
+
+// // we pass 2nd argument empty array,useEffect will run only once at starting
+
+// useEffect(()=>{
+//   console.log("inside useEffect", count1, count2);
+// },[])
+
+// return (
+//   <div>
+//   <div className="App"    onClick={()=>setcount1(count1+1)}> Counter 1:{count1}</div>
+//   <div className="App"    onClick={()=>setcount2(count2-1)}> Counter 2 :{count2}</div>
+// </div>
+// );
+// }
+// export default App;
 
 
 
 
 function App() {
-const[count1, setcount1]= useState(0);
-const[count2, setcount2]= useState(0);
+ 
+  //  fetch("https://022q7o.csb.app/todos")
 
-//useEffect will run always when any componnet mounted
-// useEffect(()=>{
-//   console.log("inside useEffect", count1, count2);
-// })
-
-
-// we pass 2nd argument empty array,useEffect will run only once at starting
-useEffect(()=>{
-  console.log("inside useEffect", count1, count2);
-},[])
-
-return (
-  <div>
-  <div className="App"    onClick={()=>setcount1(count1+1)}> Counter 1:{count1}</div>
-  <div className="App"    onClick={()=>setcount2(count2-1)}> Counter 2 :{count2}</div>
-</div>
-);
-}
-export default App;
+  fetch("http://localhost:8080/todos")
+   .then((r)=> r.json())
+   .then((d)=>{
+    console.log(d);
+  });
+ 
+  
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+  }
+  export default App;
