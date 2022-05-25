3621b2a8d6534d9d37e6507d3b23e40679bc2e59
vrkajave1994 <99703905+vrushalikajave@users.noreply.github.com>
2022-05-25 08:46:19 +0530

    commit
    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
+//   let A=()=> <div> Component : A</div>
+//   let B=()=> <div>  Component : B</div>
+
+// function App() {
+//  const[showComponent, setShowComponent]= useState()
+// // when i click on comp A - B component will mount on root level, and useeffect will rum and it will print 3.
+//  useEffect(()=>{
+//     console.log("3");
+//   })
+//   return (
+//     <div className="App"    onClick={()=>setShowComponent(!showComponent)}>  Hello
+//      {showComponent? <A/> :<B/>}
+//   </div>
+//   );
+// }
+// export default App;
+
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const[count1, setcount1]= useState(0);
+const[count2, setcount2]= useState(0);
 
+//useEffect will run always when any componnet mounted
+// useEffect(()=>{
+//   console.log("inside useEffect", count1, count2);
+// })
+
+
+// we pass 2nd argument empty array,useEffect will run only once at starting
+useEffect(()=>{
+  console.log("inside useEffect", count1, count2);
+},[])
+
+return (
+  <div>
+  <div className="App"    onClick={()=>setcount1(count1+1)}> Counter 1:{count1}</div>
+  <div className="App"    onClick={()=>setcount2(count2-1)}> Counter 2 :{count2}</div>
+</div>
+);
+}
 export default App;
